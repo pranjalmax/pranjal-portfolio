b3e49f68b714757f27fe48e3cc4dc62b87a8addb
pranjal6004 <pranjal6004@gmail.com>
2025-11-12 11:59:22 -0600

    chore: auto-unregister legacy service workers on load

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+// Auto-unregister any old service workers from legacy deploys
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then(regs => regs.forEach(r => r.unregister()))
+    .catch(() => {});
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
